0fad66bde43ab0ed86bcd9521bdeff70997ef9aa
Bekzat Molutov <b_molutov@kbtu.kz>
2022-05-29 15:20:51 +0600

    upd

--- pr2/back/cfehome/api/views.py
+++ pr2/back/cfehome/api/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from products.models import Product
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
 
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({'message': 'Hi there!'})
+    """
+    DRF API View
+    """
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        # print(instance)
+        return Response(serializer.data)
 
+
+# @api_view(["GET"])
+# def api_home(request, *args, **kwargs):
+#     """
+#     DRF API View
+#     """
+#     instance = Product.objects.all().order_by('?').first()
+#     data = {}
+#     if instance:
+#         # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+#         data = ProductSerializer(instance).data
+#     return Response(data)
--- pr2/py_client/basic.py
+++ pr2/py_client/basic.py
@@ -2,6 +2,13 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
-print(get_response.status_code)
+
+post_response = requests.post(endpoint, json={"title": "hello world"})
+print(post_response.content)
+print(post_response.status_code)
+
+
+# get_response = requests.get(endpoint)
+# print(get_response.content)
+# print(get_response.status_code)
 
